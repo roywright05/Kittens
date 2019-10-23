35f4836847a62ba46bec2d62746813ab67374862
Roy C Wright <roy.c.wright05@gmail.com>
2019-10-23 14:22:01 -0700

    Implement image button, homepage activity to rv activity, set background to a kitten themed what's app background.

--- app/src/main/java/com/example/kittens/MainActivity.java
+++ app/src/main/java/com/example/kittens/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.kittens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+   private  ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.ib_enter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                goToRvActivity();
+            }
+        });
+
+
+    }
+
+    public void goToRvActivity(){
+
+        Intent intent = new Intent(this, Main2Activity.class);
+
+        startActivity(intent);
+
     }
 }
